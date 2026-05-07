63022eb27b40ffb1039ea350f33d4dbaf39857a1
courtneylou <courtneyldelongis@gmail.com>
2026-05-07 21:44:34 +0800

    feat: add base stylesheet and resource structure

--- src/com/nook/Main.java
+++ src/com/nook/Main.java
@@ -1,6 +1,7 @@
 package com.nook;
 
 import com.nook.util.DatabaseUtil;
+import com.nook.util.NavigationUtil;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -9,7 +10,12 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         DatabaseUtil.initializeDatabase();
+        NavigationUtil.setPrimaryStage(stage);
         stage.setTitle("Nook");
+        stage.setWidth(1100);
+        stage.setHeight(750);
+        stage.setResizable(true);
+        NavigationUtil.navigateTo("/com/nook/views/login.fxml");
         stage.show();
     }
 
