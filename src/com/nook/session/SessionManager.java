27e8f1213104c4d6f9376cddf70e2c10f922589e
courtneylou <courtneyldelongis@gmail.com>
2026-05-07 21:04:22 +0800

    feat: add SessionManager

---
+++ src/com/nook/session/SessionManager.java
@@ -0,0 +1,38 @@
+package com.nook.session;
+
+import com.nook.model.User;
+
+public class SessionManager {
+
+    private static SessionManager instance;
+    private User currentUser;
+
+    private SessionManager() {}
+
+    public static SessionManager getInstance() {
+        if (instance == null) {
+            instance = new SessionManager();
+        }
+        return instance;
+    }
+
+    public void setCurrentUser(User user) {
+        this.currentUser = user;
+    }
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    public void clearSession() {
+        this.currentUser = null;
+    }
+
+    public boolean isLoggedIn() {
+        return currentUser != null;
+    }
+
+    public boolean isAdmin() {
+        return currentUser != null && currentUser.isAdmin();
+    }
+}
